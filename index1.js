e6209353d292609f557da941cbf4fa3029ad9efc
sajjad-01 <128571422+sajjad-01@users.noreply.github.com>
2024-02-06 16:24:28 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,33 @@
+// const cart = ["shoes", "pants", "watches"]
+
+// api.createOrder(cart, function () {
+
+//     api.proceedToPayement(function () {
+
+//         api.showOrderSummary(function () {
+
+//             api.updateWallet()
+
+//         })
+
+//     })
+// })
+
+
+// createOrder(cart, function(){
+//     proceedToPayement(orderId)
+// })
+
+// const promise = createOrder
+
+// // {data: orderDetails}
+
+// promise.then(function (orderId){
+//     proceedToPayement(orderId)
+// })
+
+const GITHUB_API = "https://api.github.com/users/hiteshchoudhary"
+
+const user = fetch(GITHUB_API)
+ 
+
